5ce96d989466217d1e36becf72b70f7ce989c640
sanaik2 <sanaik2@ncsu.edu>
2015-11-27 21:05:39 -0500

    test

--- build-vm/script.sh
+++ build-vm/script.sh
@@ -4,7 +4,9 @@ cd Desktop
 sudo apt-get -y install git
 sudo git clone https://github.com/SoftwareEngineeringToolDemos/FSE-2011-SCORE.git
 cd FSE-2011-SCORE/build-vm/contents
+sudo chmod +x install.sh
 sudo cp install.sh ../../../install.sh
+
 sudo cp SCORE.desktop ../../../SCORE.desktop
 
 sudo mkdir "/home/vagrant/.config/autostart"
